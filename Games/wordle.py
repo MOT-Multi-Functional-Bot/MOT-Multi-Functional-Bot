c32e7d6b1fc0bd818abf6c77d02cf8a65a9da816
Nowo <Nowo@nowo.de>
2022-02-14 13:45:39 +0100

    Made the paths cross platform usable
    os.path.join("Path", "file.sh")

--- Games/wordle.py
+++ Games/wordle.py
@@ -1,9 +1,12 @@
 import random
+import os
 
 def wordle_game():
 
     print("Ok let's play the wordle game!!!")
-    with open("Games/words.txt", "r") as file:
+
+    with open(os.path.join("Games", "words.txt"), "r") as file:
+            print(os.path.join("Games", "words.txt"))
             allText = file.read()
             word = random.choice(list(map(str, allText.split())))
     
