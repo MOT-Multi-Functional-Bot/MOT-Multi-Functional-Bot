2ce23f87d0d9d2c85774252e9d484c5bba4b672e
NatiBckr <natascha_becker@gmx.net>
2022-02-18 13:19:27 +0100

    Movigame.py small changes

--- MovieGame/moviegame.py
+++ MovieGame/moviegame.py
@@ -1,9 +1,16 @@
 import requests
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CallbackContext
+from telegram import Update, ForceReply, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CallbackContext, ConversationHandler, CommandHandler, MessageHandler, Filters
 from main_commands import log_input
 
-def movieGuessingGame(update: Update, context: CallbackContext) -> None:
-    """Send cat pic."""
+PLAYMODE, GUESS = range(2)
+
+def movieGuessingGame(update: Update, context: CallbackContext) -> int:
+    """Movie guessing Game"""
     log_input(update)
-    update.message.reply_photo()
+    update.message.reply_text("You have started the movie guessing game!\n\n" "Which playmode do you chose?", reply_markup=ReplyKeyboardMarkup, one_time_keyboard = True, input_field_placeholder = "Easy or Hard Mode?")
+    return PLAYMODE
+
+def playMode(update: Update, context: CallbackContext) -> None:
+    
+
