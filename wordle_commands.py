c0675805827a94b8be9328852072b32bb71ecfb7
Fulachs <Fulachs@users.noreply.github.com>
2022-02-18 09:51:22 +0100

    commands in different files

---
+++ wordle_commands.py
@@ -0,0 +1,13 @@
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CallbackContext
+
+def log_input(update):
+    print(str(update.message.chat_id) + " entered: " + update.message.text)
+
+def wordle(update: Update, context:CallbackContext) -> None:
+    """Play the wordle game"""
+    log_input(update)
+    update.message.reply_text("Guess words by using /guess 'word'")
+
+def guess(update: Update, context:CallbackContext) -> None:
+    pass
