b5e3ab2c429b531c91e639882cb1772525872766
Fulachs <Fulachs@users.noreply.github.com>
2022-03-11 12:38:51 +0100

    fix typo

--- conf.template.py
+++ conf.template.py
@@ -1,5 +1,5 @@
 # Go to Telegram and search "BotFather"
 # Type /start and you will get instructions on how to create a bot.
-# With the /newbot command you can create your onw Bot-API-Key
+# With the /newbot command you can create your own Bot-API-Key
 # The API-Key must be added into the following variable.
 API_KEY = ""
