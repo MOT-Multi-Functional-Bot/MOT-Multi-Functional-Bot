af8cfe8ab65d275f1b221435331445dd67da04d9
Fulachs <Fulachs@users.noreply.github.com>
2022-03-18 09:27:12 +0100

    Revert "Revert "fix directory issues #2/2""
    This reverts commit 712d86533d6348c9c22652963122c1c410799b7c.

--- games/moviegame/moviegame_commands.py
+++ games/moviegame/moviegame_commands.py
@@ -1,4 +1,4 @@
-from games.mggame.moviequiz import Quiz
+from games.moviegame.moviequiz import Quiz
 from main_commands import log_input
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import CallbackContext, ConversationHandler
